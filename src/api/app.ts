0c1dc2b181230e706b9f9fb2031b0525204fdcd9
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-05-22 09:07:01 -0400

    fix: cors

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
 import middleware from "./routes/middleware";
 import anime from "./routes/anime";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 app.use(middleware.logRequest);
+app.use(cors({
+    origin: "https://minna.gura.sa.com",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}))
 
 const routes = app
     .route('/anime', anime);
