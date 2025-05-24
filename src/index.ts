15460b88dc84329dd806ebf415b30256d7e3b12f
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-05-23 20:17:23 -0400

    style: remove commented code

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-// import { serve } from 'bun';
-// import { Hono } from "hono";
-// import 'dotenv/config';
-// import './util/cache';
-
-// import middleware from "./api/routes/middleware";
-// import anime from "./api/routes/anime";
-// import logger from './util/logger';
-
-// const app = new Hono();
-
-// app.use(middleware.logRequest);
-
-// app.route("/anime", anime);
-
-// const port = process.env.PORT ? parseInt(process.env.PORT) : 8444;
-
-// logger.info(`Starting server on port ${port}.`);
-
-// serve({
-//     port: port,
-//     fetch: app.fetch,
-// })
-
 import { serve } from 'bun';
 import 'dotenv/config';
 import './util/cache';
