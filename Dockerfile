8b4dedd5cbb5b15871cced4fbe005f3ada403cce
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-09-23 00:08:28 -0400

    reinit repo

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM oven/bun:latest
-
-RUN mkdir -p /neptune
-WORKDIR /neptune
-COPY . /neptune/
-
-RUN bun install
-
-CMD ["bun", "run", "./src/index.ts"]
--- src/api/client.ts
+++
@@ -1,6 +0,0 @@
-import { hc } from 'hono/client'
-import type { AppType } from './app';
-
-const client = hc<AppType>(`http://localhost:${process.env.PORT}`);
-
-export default client;
